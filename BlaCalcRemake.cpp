e7f58db373c41cbf36c2988859c6ec85f3d83752
Ivan <zombiegamer20100@gmail.com>
2026-07-07 14:24:27 +0200

    calc initial

---
+++ BlaCalcRemake.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float br1, br2;
+    char op;
+    cout << "Unesi prvi i drugi broj" << endl;
+    cin >> br1 >> br2;
+    cout << "Unesi operator" <<endl;
+    cin >> op;
+    switch(op){
+    case '+':
+    cout << br1 << " + " << br2 << " = " << br1 + br2;
+      break;
+
+    case '-':
+      cout << br1 << " - " << br2 << " = " << br1 - br2;
+      break;
+
+    case '*':
+      cout << br1 << " * " << br2 << " = " << br1 * br2;
+      break;
+
+    case '/':
+    if (br2==0)
+    {
+      cout << "Ne sme se deliti sa nulom!" << endl;
+      break;
+    }
+    else
+    {
+      cout << br1 << " / " << br2 << " = " << br1 / br2;
+      break;
+    }
+
+    default:
+      //ako je znak koji nije +,-,* ili / pokazuje se error poruka
+      cout << "Znak ne postoji!";
+      break;
+    }
+    return 0;
+}
