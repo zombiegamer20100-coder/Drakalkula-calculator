ca1cd4c4d27ee87edbc723da9c8e22fad744cc3c
Ivan <zombiegamer20100@gmail.com>
2026-07-12 02:12:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Drakalkula-calculator" 
--- helloworld.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main ()
-{
-    cout << "zzdravo ovo ono kao nije kalkulator blablabla" << endl;
-    return 0;
-}
